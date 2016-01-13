42e3c97e445b79ea0d5c4a53f5eb2905180e559f
Andrii Sirak <asirak@dio-soft.com>
2016-01-13 12:52:51 +0200

    fixed project setup and routes

--- jira-account-service/app/controllers/Application.java
+++ jira-account-service/app/controllers/Application.java
@@ -1,14 +1,35 @@
 package controllers;
 
 import play.*;
+import play.libs.F.Promise;
 import play.mvc.*;
-
-import views.html.*;
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Your new application is ready.");
     }
-
+    
+    public Promise<Result> getAll() {
+        return Promise.pure(internalServerError("not implemented"));
+    }
+    
+    public Promise<Result> get(String name) {
+    	return Promise.pure(internalServerError("not implemented"));
+    }
+    
+    @BodyParser.Of(BodyParser.Json.class)
+	public Promise<Result> post() {
+    	return Promise.pure(internalServerError("not implemented"));
+    }
+    
+    @BodyParser.Of(BodyParser.Json.class)
+    public Promise<Result> put(String name) {
+    	return Promise.pure(internalServerError("not implemented"));
+    }
+    
+    public Promise<Result> delete(String name) {
+    	return Promise.pure(internalServerError("not implemented"));
+    }
+    
 }
